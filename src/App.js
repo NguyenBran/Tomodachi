151a873bf2e1e15db6277a29edbbc2ffbb7dcdc6
NguyenBran <nguyen.brandon.0526@gmail.com>
2020-07-24 22:27:59 -0700

    background and pet

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import animalCrossing from './images/background/animalCrossing.jpg';
+import corgi from './images/pets/corgiSwim.gif';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='background-img' style={{backgroundImage: `url(${animalCrossing})`}}>
+      <img className='pet-img' src={corgi} />
     </div>
   );
 }
