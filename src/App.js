c29d44fa894e96b8cd16eb70258bfa1cc7b4162f
huynheddie <huynheddie@yahoo.com>
2020-07-24 23:04:25 -0700

    Added music

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
+import ReactAudioPlayer from 'react-audio-player';
 import animalCrossing from './images/background/animalCrossing.jpg';
+import animalCrossingMusic from './music/animalCrossing.mp3';
 import corgi from './images/pets/corgiSwim.gif';
 
 function App() {
+
   return (
     <div className='background-img' style={{backgroundImage: `url(${animalCrossing})`}}>
-      <img className='pet-img' src={corgi} />
+      <ReactAudioPlayer
+        src={animalCrossingMusic}
+        autoPlay={true}
+        volume={.1}
+      />
+      <img className='pet-img' src={corgi} alt='pet' />
     </div>
   );
 }
