bfefb1668155445d380b41b90bc4314a2c386300
iamdanielvong <iamdanielvong@gmail.com>
2020-07-26 15:48:16 -0700

    pineapple update

--- src/utils/pineappleGifs.js
+++ src/utils/pineappleGifs.js
@@ -1,19 +1,19 @@
 import play from '../images/pets/Pineapple/Angry_Pineapple.gif';
-import idle from '../images/pets/Pineapple/Awake_pineapple.gif';
+import talk from '../images/pets/Pineapple/Awake_pineapple.gif';
 import feed from '../images/pets/Pineapple/Eating_Pineapple.gif';
-import bathe from '../images/pets/Pineapple/Flying_Pineapple.gif';
+import idle from '../images/pets/Pineapple/Flying_Pineapple.gif';
 import happy from '../images/pets/Pineapple/Hungry_Pineapple.gif';
 import hungry from '../images/pets/Pineapple/Hungry_Pineapple.gif';
-import talk from '../images/pets/Pineapple/Love_Pineapple.gif';
+import bathe from '../images/pets/Pineapple/Love_Pineapple.gif';
 import background from '../images/background/animalCrossing.jpg';
 
 export default {
     play,
-    idle,
+    talk,
     feed,
-    bathe,
+    idle,
     happy,
     hungry,
-    talk,
+    bathe,
     background
 };
