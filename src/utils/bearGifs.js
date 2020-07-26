29e9e7ba3a28347210ae14211823bc1d21cc5a12
NguyenBran <nguyen.brandon.0526@gmail.com>
2020-07-26 11:57:49 -0700

    added files and more pet images

---
+++ src/utils/bearGifs.js
@@ -0,0 +1,15 @@
+import happy from '../images/pets/bear/happy.gif';
+import bathe from '../images/pets/bear/bathe.gif';
+import idle from '../images/pets/bear/idle.gif';
+import play from '../images/pets/bear/play.gif';
+import talk from '../images/pets/bear/talk.gif';
+import eat from '../images/pets/bear/eat.gif';
+
+export default {
+    happy,
+    eat,
+    bathe,
+    idle,
+    play,
+    talk
+};
---
+++ src/utils/pigGifs.js
@@ -0,0 +1,15 @@
+import happy from '../images/pets/pig/happy.gif';
+import eat from '../images/pets/pig/eat.gif';
+import eat2 from '../images/pets/pig/eat2.gif';
+import idle from '../images/pets/pig/idle.gif';
+import play from '../images/pets/pig/play.gif';
+import talk from '../images/pets/pig/talk.gif';
+
+export default {
+    happy,
+    eat,
+    eat2,
+    idle,
+    play,
+    talk
+};
