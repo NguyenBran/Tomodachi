cd735e396ecffc087a5e454222483e581c407f50
NguyenBran <52269954+NguyenBran@users.noreply.github.com>
2020-11-23 21:59:51 -0800

    Update login.js

--- src/services/login.js
+++ src/services/login.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // const baseUrl = 'http://localhost:3002/login';
-const baseUrl = 'https://tomodachi-backend.herokuapp.com/login'
+const baseUrl = 'https://tomodachi-backend-db.herokuapp.com/login';
 
 const login = async (credentials) => {
   const response = await axios.post(baseUrl, credentials)
